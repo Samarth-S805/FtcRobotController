29ed5faf6b178b12b9f83eec1dcc307a4b34b9b5
Samarth-S805 <samarth.k.shah07@gmail.com>
2025-11-26 18:38:41 -0500

    Added Meepmeep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariableExamples.java
@@ -7,15 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Disabled
 @TeleOp
-public class HelloWorld extends OpMode {
+public class VariableExamples {
 
-    @Override
-    public void init() {
-        telemetry.addData("Hello", "World");
-    }
 
-    @Override
-    public void loop() {
-
-    }
 }
